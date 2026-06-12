a040fad1a46801dc80727504555a96be88a8c7c3
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2026-06-12 18:09:05 -0400

    streamlined Comic model

--- src/main/java/io/hasan/comicinfoservice/models/Comic.java
+++ src/main/java/io/hasan/comicinfoservice/models/Comic.java
@@ -1,21 +1,29 @@
 package io.hasan.comicinfoservice.models;
+import java.util.UUID;
 
 public class Comic {
 
-    private String comicId;
-    private String comicName;
+    // attributes
+    private UUID id;
+    private String comicTitle;
+    private String comicStartYear;
     private String comicDesc;
 
-    public Comic(String comicId, String comicName, String comicDesc) {
-        this.comicId = comicId;
-        this.comicName = comicName;
+    // constructor
+    public Comic(UUID id, String comicTitle, String comicStartYear, String comicDesc) {
+        this.id = id;
+        this.comicTitle = comicTitle;
+        this.comicStartYear = comicStartYear;
         this.comicDesc = comicDesc;
     }
 
-    public String getComicId() {return comicId;}
-    public void setComicId(String comicId) {this.comicId = comicId;}
-    public String getComicName() {return comicName;}
-    public void setComicName(String comicName) {this.comicName = comicName;}
+    // getters and setters
+    public UUID getId() {return id;}
+    public void setId(UUID id) {this.id = id;}
+    public String getComicTitle() {return comicTitle;}
+    public void setComicTitle(String comicTitle) {this.comicTitle = comicTitle;}
+    public String getComicStartYear() {return comicStartYear;}
+    public void setComicStartYear(String comicStartYear) {this.comicStartYear = comicStartYear;}
     public String getComicDesc() {return comicDesc;}
     public void setComicDesc(String comicDesc) {this.comicDesc = comicDesc;}
 }
